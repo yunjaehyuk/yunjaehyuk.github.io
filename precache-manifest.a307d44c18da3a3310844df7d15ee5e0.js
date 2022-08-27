eb25cf79590b0877a8d24cc927143efdeac34fb1
Cane C <77560753+yunjaehyuk@users.noreply.github.com>
2022-08-27 16:40:54 +0900

    Add files via upload

---
+++ precache-manifest.a307d44c18da3a3310844df7d15ee5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4939657e44eb7a7883ca",
+    "url": "/static/css/main.8fccaf3d.chunk.css"
+  },
+  {
+    "revision": "4939657e44eb7a7883ca",
+    "url": "/static/js/main.4939657e.chunk.js"
+  },
+  {
+    "revision": "642af11f2d901ec30689",
+    "url": "/static/css/1.c500c683.chunk.css"
+  },
+  {
+    "revision": "642af11f2d901ec30689",
+    "url": "/static/js/1.642af11f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58d74e0d25c529dd22ff28372e8a9384",
+    "url": "/static/media/messi.58d74e0d.jpg"
+  },
+  {
+    "revision": "2cdc9e1bfa3da39cfb39b4cd961d0ca2",
+    "url": "/index.html"
+  }
+];
